da6e00ef8bbc121c86b43d0d52d9914b6fbc84a9
rmwdcsur <risso.santiago@gmail.com>
2026-01-10 20:31:43 -0300

    added a 2nd route and controller file

---
+++ controllers/nathaliaController.js
@@ -0,0 +1,6 @@
+// Controller function
+const getNathalia = (req, res) => {
+    res.send('Nathalia Gonzalez.');
+};
+
+module.exports = { getNathalia };
--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,9 @@ const homeController = require('../controllers/homeController');
 // Call controller function
 router.get('/', homeController.getHome);
 
+//call nathalia controller function
+const nathaliaController = require('../controllers/nathaliaController');
+router.get('/nathalia', nathaliaController.getNathalia);
+
 
 module.exports = router;
