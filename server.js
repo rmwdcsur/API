c6300de69c65a8defcd939d821b756c4d195dd49
rmwdcsur <risso.santiago@gmail.com>
2026-01-31 21:09:40 -0300

    added cors and swagger

--- server.js
+++ server.js
@@ -1,11 +1,27 @@
 const express = require("express");
 const connectDB = require("./DB/mongoDB");
+const swaggerAutogen = require("swagger-autogen")();
+
+const router = require("express").Router();
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
 
 const app = express();
 const PORT = 3000;
 
 // Connect to MongoDB
 connectDB();
+
+// Allow cors origins
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept",
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  next();
+});
 
 // Middleware to parse JSON bodies
 app.use(express.json());
@@ -15,6 +31,9 @@ const routes = require("./routes/index");
 
 // Use routes
 app.use("/", routes);
+
+// Serve Swagger UI properly
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
---
+++ swagger.js
@@ -0,0 +1,15 @@
+const swaggerAutogen = require("swagger-autogen")();
+
+const doc = {
+  info: {
+    title: "My Contacts API",
+    description: "Automatically generated Swagger documentation",
+  },
+  host: "localhost:3000",
+  schemes: ["http"],
+};
+
+const outputFile = "./swagger.json";
+const endpointsFiles = ["./routes/index.js"];
+
+swaggerAutogen(outputFile, endpointsFiles, doc);
